793a83e0c7ab1917c8db36a29272328104aba3da
MattYamamoto <matt.yamamoto@gmail.com>
2020-01-24 21:44:05 -0800

    add: Distance function with tests

--- cSharp/exercises/hamming/Hamming.cs
+++ cSharp/exercises/hamming/Hamming.cs
@@ -2,8 +2,28 @@ using System;
 
 public static class Hamming
 {
-    public static int Distance(string firstStrand, string secondStrand)
+  public static int Distance(string firstStrand, string secondStrand)
+  {
+    // Check if we are allowed to calculate Hamming distance.
+    if (firstStrand.Length != secondStrand.Length)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      throw new System.ArgumentException("Strands must be of equal length.");
     }
+
+    // If strands are identical, there is no Hamming distance.
+    if (firstStrand == secondStrand)
+    {
+      return 0;
+    }
+
+    // Calculate hamming distance.
+    int dist = 0;
+    for (int i = 0; i < firstStrand.Length; i++)
+    {
+      if (firstStrand[i] != secondStrand[i])
+        dist++;
+    }
+
+    return dist;
+  }
 }
