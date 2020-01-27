7f53aaac3a61f12ebd83f1a67ff9715a4801da5a
MattYamamoto <matt.yamamoto@gmail.com>
2020-01-26 17:53:23 -0800

    add: collatz conjecture solution

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -2,8 +2,20 @@
 
 public static class CollatzConjecture
 {
-    public static int Steps(int number)
+  public static int Steps(int number)
+  {
+    if (number <= 0)
     {
-        throw new NotImplementedException("You need to implement this function");
+      throw new ArgumentException("Argument must be a positive integer");
     }
+
+    if (number == 1)
+    {
+      return 0;
+    }
+
+    int nextNumber = number % 2 == 0 ? number / 2 : 3 * number + 1;
+
+    return Steps(nextNumber) + 1;
+  }
 }
