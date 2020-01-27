8d25c0289d41a366e47ca0bfd5d7189c7ead8599
MattYamamoto <matt.yamamoto@gmail.com>
2020-01-26 19:37:10 -0800

    add: phone number solution

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
+  public const string pattern = @"^\D*1?\D*([2-9]\d{2})\D*([2-9]\d{2})\D*(\d{4})$";
   public static string Clean(string phoneNumber)
   {
-    throw new NotImplementedException("You need to implement this function.");
+
+    Match m = Regex.Match(phoneNumber.Trim(), pattern);
+
+    if (!m.Success)
+    {
+      throw new ArgumentException("Not a valid NANP phone number");
+    }
+
+    string cleanNumber = m.Groups[1].Value + m.Groups[2].Value + m.Groups[3].Value;
+
+    return cleanNumber;
   }
 }
