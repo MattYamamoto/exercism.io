3d2942089f9be3d6725eee38de3ccb359456ed1d
MattYamamoto <matt.yamamoto@gmail.com>
2020-01-30 21:44:44 -0800

    add: initial triangle solution

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -2,18 +2,34 @@ using System;
 
 public static class Triangle
 {
+  public static bool IsValidTriangle(double side1, double side2, double side3)
+  {
+    if (side1 == 0 || side2 == 0 || side3 == 0) return false;
+
+    double sum12 = side1 + side2;
+    double sum13 = side1 + side3;
+    double sum23 = side2 + side3;
+
+    if (sum12 < side3 || sum13 < side2 || sum23 < side1) return false;
+
+    return true;
+  }
   public static bool IsScalene(double side1, double side2, double side3)
   {
-    throw new NotImplementedException("You need to implement this function.");
+    if (!IsValidTriangle(side1, side2, side3)) return false;
+
+    return side1 != side2 && side1 != side3 && side2 != side3;
   }
 
   public static bool IsIsosceles(double side1, double side2, double side3)
   {
-    throw new NotImplementedException("You need to implement this function.");
+    if (!IsValidTriangle(side1, side2, side3)) return false;
+
+    return !(side1 != side2 && side1 != side3 && side2 != side3);
   }
 
   public static bool IsEquilateral(double side1, double side2, double side3)
   {
-    throw new NotImplementedException("You need to implement this function.");
+    return side1 > 0 && side1 == side2 && side2 == side3;
   }
 }
