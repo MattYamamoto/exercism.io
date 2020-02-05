1530d12d8aedbb0a96f8f58840d8e077eeb3bf71
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-04 23:43:20 -0800

    add: acronym solution

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        string pattern = @"[^A-Za-z']([A-Za-z])";
+        Regex re = new Regex(pattern);
+
+        var startLetters = re.Matches(" " + phrase)
+                .Cast<Match>()
+                .Select(m => m.Groups[1].Value)
+                .ToArray();
+
+        return String.Join("", startLetters).ToUpper();
     }
 }
