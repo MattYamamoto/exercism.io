0177c5289c09744452db32b466850c6d5bbf6235
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-05 22:59:02 -0800

    add: protein translation solution

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ProteinTranslation
 {
+    public static IDictionary<string, string> peptides = new Dictionary<string, string>() {
+          {"AUG", "Methionine"}
+        , {"UUU", "Phenylalanine"}
+        , {"UUC", "Phenylalanine"}
+        , {"UUA", "Leucine"}
+        , {"UUG", "Leucine"}
+        , {"UCU", "Serine"}
+        , {"UCC", "Serine"}
+        , {"UCA", "Serine"}
+        , {"UCG", "Serine"}
+        , {"UAU", "Tyrosine"}
+        , {"UAC", "Tyrosine"}
+        , {"UGU", "Cysteine"}
+        , {"UGC", "Cysteine"}
+        , {"UGG", "Tryptophan"}
+        , {"UAA", ""}
+        , {"UAG", ""}
+        , {"UGA", ""}
+    };
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException();
+        var translation = new List<string>();
+
+        for (int i = 0; i < strand.Length; i += 3)
+        {
+            string codon = strand.Substring(i, 3);
+            string peptide = peptides[codon];
+            if (String.IsNullOrEmpty(peptide))
+            {
+                break;
+            }
+
+            translation.Add(peptide);
+        }
+        return translation.ToArray();
     }
 }
