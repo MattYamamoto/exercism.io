14dd99deff7fa22f023c38fda61b0aebb0b00bbc
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-07 20:24:02 -0800

    add: resistor color solution

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ResistorColor
 {
+    public static IDictionary<string, int> colorValues = new Dictionary<string, int>
+    {
+          {"black"  , 0}
+        , {"brown"  , 1}
+        , {"red"    , 2}
+        , {"orange" , 3}
+        , {"yellow" , 4}
+        , {"green"  , 5}
+        , {"blue"   , 6}
+        , {"violet" , 7}
+        , {"grey"   , 8}
+        , {"white"  , 9}
+    };
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return colorValues[color];
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return colorValues.Keys.ToArray();
     }
 }
