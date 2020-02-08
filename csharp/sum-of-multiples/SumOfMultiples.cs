1e47c09a8210c29992209df0d3207aaeffefad48
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-07 23:12:54 -0800

    add: sum of multiples solution

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> multiples, int max)
+    public static int Sum(IEnumerable<int> baseNumbers, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        return baseNumbers
+            .SelectMany(n => allMultiples(n, max))
+            .Distinct()
+            .Sum();
+    }
+
+    public static IEnumerable<int> allMultiples(int baseNumber, int max)
+    {
+        int multiple = baseNumber;
+        IList<int> multiples = new List<int>();
+
+        if (baseNumber == 0)
+        {
+            return multiples;
+        }
+
+        while (multiple < max)
+        {
+            multiples.Add(multiple);
+
+            multiple += baseNumber;
+        }
+        return multiples;
     }
 }
