55288c5908061fcfdf73ee5f4d34527b13f3d118
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-09 22:14:52 -0800

    add: grains solution

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -4,11 +4,23 @@ public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (n < 1 || n > 64)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return Convert.ToUInt64(Math.Pow(2, n - 1));
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        ulong total = 0;
+
+        for (int i = 1; i <= 64; i++)
+        {
+            total += Square(i);
+        }
+
+        return total;
     }
 }
