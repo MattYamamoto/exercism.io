a714708cef34e0bbd05677ae6c555c60a8c147f6
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-12 22:14:03 -0800

    add: perfect numbers solution

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -11,6 +11,22 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException();
+        if (number < 1) throw new ArgumentOutOfRangeException();
+
+        int aliquotSum = 0;
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                aliquotSum += i;
+            }
+        }
+
+        return aliquotSum == number
+            ? Classification.Perfect
+            : aliquotSum > number
+                ? Classification.Abundant
+                : Classification.Deficient;
     }
+
 }
