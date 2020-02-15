44fcebf0cc5b9ced13c763734247c73cf1aef72c
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-15 13:17:37 -0800

    add: largest series product solution

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Linq;
 
 public static class LargestSeriesProduct
 {
-    public static long GetLargestProduct(string digits, int span) 
+    public static long GetLargestProduct(string digits, int span)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (digits.Length < span
+            || span < 0
+            || !digits.All(Char.IsDigit)
+           )
+        {
+            throw new ArgumentException();
+        }
+
+        long maxProduct = 0;
+        long product;
+        for (int i = 0; i <= digits.Length - span; i++)
+        {
+            product = GetProduct(digits.Substring(i, span));
+            maxProduct = Math.Max(product, maxProduct);
+        }
+
+        return maxProduct;
+    }
+
+    public static long GetProduct(string digits)
+    {
+        long product = digits.Aggregate(1L, (p, c) =>
+        {
+            p *= (long)Char.GetNumericValue(c);
+            return p;
+        });
+
+        return product;
     }
 }
