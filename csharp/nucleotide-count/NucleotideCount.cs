5fd3f9898548dcd55852a70798b88c8ac81beb4b
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-17 10:07:54 -0800

    add: nucleotide count solution

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Regex re = new Regex(@"[^ACGT]");
+
+        if (re.IsMatch(sequence)) throw new ArgumentException();
+
+        IDictionary<char, int> counts = new Dictionary<char, int>(){
+              {'A', sequence.Count(c => c == 'A')}
+            , {'C', sequence.Count(c => c == 'C')}
+            , {'G', sequence.Count(c => c == 'G')}
+            , {'T', sequence.Count(c => c == 'T')}
+        };
+
+        return counts;
     }
 }
