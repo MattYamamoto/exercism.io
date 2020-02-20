3e0e9e12f063cd6f8094db623f67db586adf8ecf
Matt Yamamoto <matt.yamamoto@gmail.com>
2020-02-19 20:26:12 -0800

    feat: naive solution to pythagorean triplet

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,27 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        List<(int, int, int)> triplets = new List<(int, int, int)>();
+
+        int a;
+        int b;
+        int c;
+
+        for (a = 1; a < sum / 2; a++)
+        {
+            for (b = a; b < sum / 2; b++)
+            {
+                c = sum - (a + b);
+
+                if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                {
+                    triplets.Add((a, b, c));
+                }
+            }
+
+        }
+
+        return triplets;
     }
 }
