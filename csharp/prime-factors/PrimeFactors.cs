162b5b3a2ed862614d97aca047bd1299e2e88692
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-23 21:12:38 -0800

    feature: prime factors solution

--- csharp/prime-factors/PrimeFactors.cs
+++ csharp/prime-factors/PrimeFactors.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
     public static long[] Factors(long number)
     {
-        throw new NotImplementedException();
+
+        List<long> factors = new List<long>();
+
+        long divisor = number;
+        long factor = 2;
+
+        while (divisor != 1)
+        {
+            if (divisor % factor == 0)
+            {
+                factors.Add(factor);
+                divisor /= factor;
+            }
+            else
+            {
+                factor++;
+            }
+        }
+
+        return factors.ToArray();
     }
 }
