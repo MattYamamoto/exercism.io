82b31c88d4d7d846d3055980b8407bfe5a73b320
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-24 22:31:41 -0800

    wip: need unique names

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
+    public static HashSet<string> names = new HashSet<string>();
+
+    private string _name;
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (String.IsNullOrEmpty(_name))
+            {
+                Reset();
+            }
+            return _name;
         }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Random r = new Random();
+
+        int attempt = 1;
+        string tryName = "";
+        do
+        {
+            tryName = ((char)('A' + r.Next(0, 26))).ToString()
+                + ((char)('A' + r.Next(0, 26))).ToString()
+                + r.Next(0, 1000).ToString("D3");
+
+            attempt++;
+
+        } while (attempt < 26 * 26 * 1000 && names.Add(tryName));
+
+
     }
 }
