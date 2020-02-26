14aadf20cdf8badec0ce4f94eabe68b556247eca
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-25 23:15:53 -0800

    feat: difference of squares solution

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = Enumerable.Range(1, max)
+            .Aggregate(0, (runningTotal, currentNumber) => runningTotal + currentNumber);
+
+        return sum * sum;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> squares = Enumerable.Range(1, max)
+            .Select(currentNumber => currentNumber * currentNumber).ToList();
+
+        return squares.Aggregate(0, (runningTotal, currentNumber) => runningTotal + currentNumber);
     }
 
-    public static int CalculateDifferenceOfSquares(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+
 }
