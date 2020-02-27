a2fd6769204151231928d8b949fce0c0cd625848
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-26 22:23:59 -0800

    feat: ETL solution

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -1,10 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Dictionary<string, int> transformed = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<int, string[]> oldEntry in old)
+        {
+            foreach (string letter in oldEntry.Value)
+            {
+                transformed.Add(letter.ToLower(), oldEntry.Key);
+            }
+        }
+
+        return transformed
+            .OrderByDescending(KeyValuePair => KeyValuePair.Key)
+            .ToDictionary(KeyValuePair => KeyValuePair.Key, KeyValuePair => KeyValuePair.Value);
     }
 }
