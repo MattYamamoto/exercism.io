2582a428f1cbacc9332a5037fafd0a0d92c97318
MattYamamoto <matt.yamamoto@gmail.com>
2020-02-27 22:27:29 -0800

    feat: unique robot names

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 public class Robot
 {
     public static HashSet<string> names = new HashSet<string>();
+
+    private Random random = new Random();
 
     private string _name;
     public string Name
@@ -20,20 +23,36 @@ public class Robot
 
     public void Reset()
     {
-        Random r = new Random();
 
-        int attempt = 1;
+        int attempt = 0;
+        bool isUniqueName;
         string tryName = "";
         do
         {
-            tryName = ((char)('A' + r.Next(0, 26))).ToString()
-                + ((char)('A' + r.Next(0, 26))).ToString()
-                + r.Next(0, 1000).ToString("D3");
+            tryName = randomLetters(2) + randomNumericString(3);
+            isUniqueName = names.Add(tryName);
 
             attempt++;
 
-        } while (attempt < 26 * 26 * 1000 && names.Add(tryName));
+        } while (attempt < 26 * 26 * 1000 && !isUniqueName);
 
+        _name = tryName;
+    }
 
+    private string randomLetters(int numberOfLetters)
+    {
+        string letters = "";
+
+        for (int i = 0; i < numberOfLetters; i++)
+        {
+            letters += ((char)('A' + random.Next(0, 26))).ToString();
+        }
+
+        return letters;
+    }
+
+    private string randomNumericString(int digits)
+    {
+        return random.Next(0, (int)Math.Pow(10, digits)).ToString("D" + digits.ToString());
     }
 }
